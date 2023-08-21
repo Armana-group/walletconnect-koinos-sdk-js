7207419943267a2137ca8caf4e4226c75b0eaf96
Roamin <roamin@tuta.io>
2023-08-21 19:16:22 +0000

    add network selector

--- demo/demo.ts
+++ demo/demo.ts
@@ -66,7 +66,7 @@ async function onConnect(): Promise<void> {
   try {
     connectButton.disabled = true
     accounts = await walletConnectKoinos.connect(
-      [ChainIds.Harbinger],
+      [ChainIds.Harbinger, ChainIds.Mainnet],
       [Methods.SignMessage, Methods.SignTransaction]
     )
     console.info(accounts)
@@ -91,10 +91,19 @@ async function onDisconnect(): Promise<void> {
   }
 }
 
+function getNetworkSelection() {
+  const networkSelection = (document.querySelector('input[name="network"]:checked') as HTMLInputElement)?.value
+  console.log(networkSelection)
+  
+  return networkSelection === 'testnet' ? ChainIds.Harbinger : ChainIds.Mainnet
+}
+
 async function onSignMessage(): Promise<void> {
   try {
     signMessageButton.disabled = true
-    const signer = walletConnectKoinos.getSigner(accounts[0])
+    const network = getNetworkSelection()
+
+    const signer = walletConnectKoinos.getSigner(accounts[0], undefined, network)
     const signature = await signer.signMessage(messageInput.value)
     messageSignatureInput.value = utils.encodeBase64url(signature)
   } catch (err) {
@@ -108,13 +117,17 @@ async function onSignMessage(): Promise<void> {
 async function onSignTransaction(): Promise<void> {
   try {
     signTransactionButton.disabled = true
-    const provider = new Provider('https://harbinger-api.koinos.io')
-    const signer = walletConnectKoinos.getSigner(accounts[0], provider)
+    const network = getNetworkSelection()
+    const jsonRPCUrl = network === ChainIds.Harbinger ? 'https://harbinger-api.koinos.io' : 'https://api.koinos.io'
+    
+    const provider = new Provider(jsonRPCUrl)
+    const signer = walletConnectKoinos.getSigner(accounts[0], provider, network)
 
+    const koinContractId = network === ChainIds.Harbinger ? '1FaSvLjQJsCJKq5ybmGsMMQs8RQYyVv8ju' : '15DJN4a8SgrbGhhGksSBASiSYjGnMU8dGL'
+    console.log(network)
     // get Koin balance
     const koin = new Contract({
-      // Harbinger Testnet Koin contract
-      id: '1FaSvLjQJsCJKq5ybmGsMMQs8RQYyVv8ju',
+      id: koinContractId,
       abi: utils.tokenAbi,
       signer
     })
