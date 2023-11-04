ecbd720ddf419dba788a11dd23e2702657ec678e
Roamin <roamin@tuta.io>
2023-11-04 23:34:35 +0000

    fix package generation

--- vite.config.ts
+++ vite.config.ts
@@ -1,23 +1,24 @@
-import { resolve } from "path";
-import { defineConfig } from "vite";
-import dts from "vite-plugin-dts";
+import { resolve } from 'path'
+import { defineConfig } from 'vite'
+import dts from 'vite-plugin-dts'
 
 export default defineConfig({
   server: {
-    port: 3000,
+    port: 3000
   },
   build: {
     lib: {
-      entry: resolve(__dirname, "src/index.ts"),
-      name: "walletconnect-koinos-sdk-js",
-      fileName: "walletconnect-koinos-sdk-js",
+      entry: resolve(__dirname, 'src/index.ts'),
+      name: 'walletconnect-koinos-sdk-js',
+      fileName: 'walletconnect-koinos-sdk-js',
+      formats: ['cjs', 'es']
     },
     rollupOptions: {
       external: [],
       output: {
-        globals: {},
-      },
-    },
+        globals: {}
+      }
+    }
   },
-  plugins: [dts()],
-});
+  plugins: [dts()]
+})
